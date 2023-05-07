e8f65915514305d4806abe6057dd7be0899286e2
George Njau Ngugi <gngugi@copiakenya.com>
2023-05-08 00:41:56 +0300

    add comment

--- samplebcptest.js
+++ samplebcptest.js
@@ -8,6 +8,7 @@ export const options = {
       apm: [],
     },
   },
+  // adding comments to the codess
   thresholds: {
     http_req_duration: ['p(90)<40000'], // 90% of requests must complete below 40s
     http_req_failed : ['rate<0.03'],   // the error rate must be lower than 3%
