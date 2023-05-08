c464eb23774aaedecf6abb7463384c381f7ffbd9
George Njau Ngugi <gngugi@copiakenya.com>
2023-05-08 10:26:09 +0300

    Test

--- samplebcptest.js
+++ samplebcptest.js
@@ -8,7 +8,7 @@ export const options = {
       apm: [],
     },
   },
-  // adding comments to verify the jenkins github integration
+  // adding comments to verify the jenkins github integratqqw
   thresholds: {
     http_req_duration: ['p(90)<40000'], // 90% of requests must complete below 40s
     http_req_failed : ['rate<0.03'],   // the error rate must be lower than 3%
